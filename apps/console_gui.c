545d7810ed73209d6d97fda8dff71875033a30be
Davide <davidepianca98@gmail.com>
2016-11-12 18:51:10 +0100

    Use console_print

--- apps/console_gui.c
+++ apps/console_gui.c
@@ -40,21 +40,9 @@ void console_init_gui(char *usr) {
     text_area_set_text(text_area, "Console started\n");
     
     user = kmalloc(sizeof(strlen(usr) + 1));
-    memset(user, 0, strlen(usr) + 1);
     strcpy(user, usr);
     console_run_gui();
     kfree(user);
-}
-
-char str[256];
-
-char *to_string(char *buffer, ...) {
-    va_list args;
-    memset(str, 0, 256);
-    va_start(args, buffer);
-    vsprintf(str, buffer, args);
-    va_end(args);
-    return str;
 }
 
 /**
@@ -64,7 +52,7 @@ void console_run_gui() {
     char *buffer = kmalloc(64);
     char c = 0;
     
-    text_area_append(text_area, to_string("%s  $ ", user));
+    console_print("%s  $ ", user);
     int buffer_counter = 0;
     while(1) {
         c = keyboard_get_lastkey();
@@ -80,14 +68,13 @@ void console_run_gui() {
                 continue;
             }
         }
-        str[0] = c;
-        str[1] = 0;
-        text_area_append(text_area, str);
+
+        console_print("%c", c);
         if(c == '\n') { // Enter pressed
             buffer[buffer_counter] = '\0';
             console_exec(buffer);
             buffer_counter = 0;
-            text_area_append(text_area, to_string("%s %s $ ", user, get_dir()));
+            console_print("%s %s $ ", user, get_dir());
         }
     }
     kfree(buffer);
